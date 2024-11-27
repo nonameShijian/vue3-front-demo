ca126b0e13edbe351aea1095807a6be953197695
诗笺 <2954700422@qq.com>
2024-11-27 10:55:27 +0800

    添加NotFound的拦截，格式化代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,19 +4,24 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
-		  path: '/',
-		  name: '',
-		  component: () => import('../views/HomePage.vue')
+			path: '/',
+			name: '',
+			component: () => import('../views/HomePage.vue'),
 		},
 		{
-		  path: '/login',
-		  name: 'login',
-		  component: () => import('../views/LoginPage.vue')
+			path: '/login',
+			name: 'login',
+			component: () => import('../views/LoginPage.vue'),
 		},
 		{
-		  path: '/register',
-		  name: 'register',
-		  component: () => import('../views/RegisterPage.vue')
+			path: '/register',
+			name: 'register',
+			component: () => import('../views/RegisterPage.vue'),
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'notFound',
+			component: () => import('../views/NotFoundPage.vue'),
 		},
 	],
 });
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,50 +1,46 @@
 <template>
-    <div>登录页</div>
-    <el-form ref="formRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="用户名" prop="username">
-            <el-input v-model="form.username"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-            <el-input v-model="form.password" type="password"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="login">登录</el-button>
-        </el-form-item>
-    </el-form>
+	<div>登录页</div>
+	<el-form ref="formRef" :model="form" :rules="rules" label-width="80px">
+		<el-form-item label="用户名" prop="username">
+			<el-input v-model="form.username"></el-input>
+		</el-form-item>
+		<el-form-item label="密码" prop="password">
+			<el-input v-model="form.password" type="password"></el-input>
+		</el-form-item>
+		<el-form-item>
+			<el-button type="primary" @click="login">登录</el-button>
+		</el-form-item>
+	</el-form>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue";
-import { ElForm, ElMessage } from "element-plus";
+import { reactive, ref } from 'vue';
+import { ElForm, ElMessage } from 'element-plus';
 
 const formRef = ref<InstanceType<typeof ElForm>>();
 
 const form = reactive({
-    username: "",
-    password: ""
+	username: '',
+	password: '',
 });
 
 // 校验规则
 const rules = {
-    username: [
-        { required: true, message: "请输入用户名", trigger: "blur" }
-    ],
-    password: [
-        { required: true, message: "请输入密码", trigger: "blur" }
-    ]
+	username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+	password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
 };
 
 const login = () => {
-    if (formRef.value) {
-        formRef.value.validate((valid) => {
-            if (valid) {
-                console.log(form);
-                // 登录逻辑
-            } else {
-                ElMessage.error("校验失败");
-            }
-        });
-    }
+	if (formRef.value) {
+		formRef.value.validate((valid) => {
+			if (valid) {
+				console.log(form);
+				// 登录逻辑
+			} else {
+				ElMessage.error('校验失败');
+			}
+		});
+	}
 };
 </script>
 
